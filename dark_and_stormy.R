053f1ff9918be1bf156c73394c480e538d565f97
lyao3515 <38146756+lyao3515@users.noreply.github.com>
2020-03-05 12:33:44 -0500

    Update dark_and_stormy.R

--- dark_and_stormy.R
+++ dark_and_stormy.R
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 print("It was a dark and stormy night; the rain fell in torrents.")
-=======
-print("It was a dark and stormy night; the rain fell in torrents."
->>>>>>> 89605d982d4dd29ab42d1c61974d641b79c86b6c
